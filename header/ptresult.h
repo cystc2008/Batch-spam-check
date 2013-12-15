105c4da0a020c2e50de000125ddc056b529cf6f0
cystc2008 <cystc2008@hotmail.com>
2013-12-15 10:46:53 +0800

    bgfh

---
+++ header/ptresult.h
@@ -0,0 +1,9 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDD_RESULT                              101
+#define IDR_MENU                                103
+#define IDM___1									40000
+#define IDC_SAVE                                1001
+#define IDC_RESULT                              1003
---
+++ header/resource.h
@@ -0,0 +1,12 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDI_ICON1                               103
+#define IDI_ICON2                               104
+#define IDI_ICON3                               105
+#define MAIN_GUI                                106
+#define IDABOUT                                 1000
+#define IDCHECK                                 1001
+#define IP_LIST                                 1002
+#define IDCLEAN                                 1005
