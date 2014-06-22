4672334d669bb735ebafeccb200178ff0dabb248
cystc2008 <cystc2008@hotmail.com>
2014-06-23 00:47:59 +0800

    fix bug

--- header/ptresult.h
+++ header/ptresult.h
@@ -4,9 +4,9 @@
 
 #define IDD_RESULT                              101
 #define IDR_MENU                                103
-#define IDM___1	                                40000
-#define IDM___2                                 40001
-#define IDM___SORT1                             40002
-#define IDM___SORT2                             40003
+#define IDM_COPY	                            40000
+#define IDM_COPYIP                              40001
+#define IDM_SORT1                               40002
+#define IDM_SORT2                               40003
 #define IDC_SAVE                                1001
 #define IDC_RESULT                              1003
